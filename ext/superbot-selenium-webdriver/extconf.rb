2c969fd58d6475225f49228f123f6e0b5bbaafe2
Matti Paksula <matti.paksula@iki.fi>
2018-12-14 21:25:32 +0200

    fix lib load paths

--- ext/superbot-selenium-webdriver/extconf.rb
+++ ext/superbot-selenium-webdriver/extconf.rb
@@ -1,4 +1,9 @@
+
+lib_dir = File.expand_path('../../lib', File.dirname(__FILE__))
+$LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
+
 require "superbot/selenium/webdriver"
+
 Superbot::Selenium::WebDriver.install_chromedriver_helper!
 
 require "mkmf"
--- lib/superbot/selenium/version.rb
+++ lib/superbot/selenium/version.rb
@@ -1,7 +1,7 @@
 module Superbot
   module Selenium
     module WebDriver
-      VERSION = "1.0.4"
+      VERSION = "1.0.5"
     end
   end
 end
